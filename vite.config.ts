e74d69ed79e241c9283e10f19cec42bea5143470
Ilia Riabsev <46lidiay@gmail.com>
2025-12-09 23:37:12 +0200

    Changed Vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,19 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/client-scripts-online/',
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: undefined, 
+      },
+    },
+  },
+  worker: {
+    format: 'es',
+    rollupOptions: {
+      output: {
+        format: 'es',
+      },
+    },
+  },
 })
